9bf6dd7b947643779ae81218fdc6553245813030
bobycat22 <anthonyznj@gmail.com>
2021-06-12 09:04:22 -0400

    try again lol

--- django_medcheck/core/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-
-from core import views
-
-urlpatterns = []
--- django_medcheck/django_medcheck/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.authtoken')),
-    path('api/v1/', include('core.urls')),
-]
